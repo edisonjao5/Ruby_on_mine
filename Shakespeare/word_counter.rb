c40ecfe72a3734a973c8d2c02714ed51610b8d81
Edison Jao <coded5@icloud.com>
2023-07-31 01:54:19 -0400

    Some improvements in the iteration and getting the final words

--- Shakespeare/word_counter.rb
+++ Shakespeare/word_counter.rb
@@ -2,7 +2,12 @@ FILE_NAME = "./Shakespeare/romeo-juliet.txt"
 
 #load the words from the file
 def words_from_file(file_name)
-  File.read(file_name).downcase.gsub(/[^a-z]/, " ").split
+  begin # you can omit begin and end if you want just use rescue
+    File.read(file_name).downcase.gsub(/[^a-z]/, " ").split
+  rescue
+    puts "Could not open #{file_name}"
+    exit
+  end
 end
 
 #load the list of words
@@ -10,13 +15,23 @@ words = words_from_file(FILE_NAME)
 # puts words
 
 #create a hash to store the word count
-word_count = {}
+WORD_COUNT = {}
 
 #then we are going to iterate through the words
 words.each do |word|
-  if word_count[word]
-    word_count[word] += 1
+=begin
+  if WORD_COUNT[word]
+    WORD_COUNT[word] += 1
   else
-    word_count[word] = 1
+    WORD_COUNT[word] = 1
   end
+=end
+  WORD_COUNT[word] = 0 unless WORD_COUNT[word]
+  WORD_COUNT[word] += 1
 end
+
+#sort by count and reverse it
+WORD_COUNT.sort_by {|word, count| count}
+          .reverse[0...20]
+          .each {|word, count| puts "#{word}: #{count}"}
+
